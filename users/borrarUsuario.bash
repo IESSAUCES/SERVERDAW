d5c412f2f555e0337a800ca490a8e16b010bd77e
bea93 <bmerinomacia@gmail.com>
2020-12-11 10:13:52 +0000

    Modificación scripts de creación y borrado de usuarios

--- users/borrarUsuario.bash
+++ users/borrarUsuario.bash
@@ -63,16 +63,16 @@ while [ $CURSO -lt 3 ]; do
 			fi
 
 #Deshabilitamos el sitio en apache
-#a2dissite "$USUARIO.conf"
+a2dissite "$USUARIO.conf"
 
 #Eliminamos la configuracion del sitio 
-#rm /etc/apache2/sites-available/"$USUARIO".conf
+rm /etc/apache2/sites-available/"$USUARIO".conf
 
 
 	echo "Usuario $USUARIO eliminado correctamente!"
 done
 done
-
+/etc/init.d/apache2 restart
 
 
 
